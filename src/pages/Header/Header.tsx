17dc637a9508e24efbd04438227f34a20bf8b4a5
imaikosuke <imaikosuke2@gmail.com>
2024-02-19 19:01:34 +0900

    ログイン状態によって、Headerの表示を変更

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -9,18 +9,20 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
       <h1 className="text-2xl font-bold">Todoリスト</h1>
-      <div className="flex items-center">
-        <p className="mr-3">
-          <strong>
-            <span>{user?.email}</span>
-          </strong>
-        </p>
-        <img
-          src={user?.photoURL || '../../assets/default_user_icon.png'}
-          alt="Googleアカウントのアイコン"
-          className="w-10 h-10 rounded-full"
-        />
-      </div>
+      {user ? (
+        <div className="flex items-center">
+          <p className="mr-3">
+            <strong>
+              <span>{user.email}</span>
+            </strong>
+          </p>
+          <img
+            src={user.photoURL || ''}
+            alt="Googleアカウントのアイコン"
+            className="w-10 h-10 rounded-full"
+          />
+        </div>
+      ) : null}
     </header>
   );
 };
