b71184234a3fa1a6e27385d27b72f4a88ddbf313
imaikosuke <imaikosuke2@gmail.com>
2024-02-22 23:08:12 +0900

    Login画面のテストを実装

---
+++ src/pages/Login.test.tsx
@@ -0,0 +1,30 @@
+// src/pages/Login.test.tsx
+
+import { render, screen } from '@testing-library/react';
+import Login from './Login';
+
+jest.mock('../firebase', () => ({
+  auth: jest.fn(),
+  provider: jest.fn(),
+}));
+
+describe('Login画面のテスト', () => {
+  beforeEach(() => {
+    render(<Login />);
+  });
+
+  test('Todoリストというタイトルがある', () => {
+    const title = screen.getByRole('heading', { name: 'Todoリスト' });
+    expect(title).toBeInTheDocument();
+  });
+  
+  test('始めるにはGoogleアカウントによるログインが必要です。というテキストがある', () => {
+    const text = screen.getByText('始めるにはGoogleアカウントによるログインが必要です。');
+    expect(text).toBeInTheDocument();
+  });
+  
+  test('Googleアカウントでログインというボタンがある', () => {
+    const loginButton = screen.getByRole('button', { name: 'Googleアカウントでログイン' });
+    expect(loginButton).toBeInTheDocument();
+  });
+});
